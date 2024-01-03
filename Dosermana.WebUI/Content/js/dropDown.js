9eeb0d5fcb062e4d40213b707fd55590fe2c3410
poireau49128 <kovalkov1899@mail.ru>
2024-01-03 15:53:09 +0300

    Почти весь дизайн

---
+++ Dosermana.WebUI/Content/js/dropDown.js
@@ -0,0 +1,13 @@
+﻿function toggleDropdown() {
+    var dropdownMenu = document.getElementById("dropdownMenu");
+    dropdownMenu.style.display = dropdownMenu.style.display === "none" ? "block" : "none";
+}
+
+document.addEventListener("click", function (event) {
+    var dropdownMenu = document.getElementById("dropdownMenu");
+    var targetElement = event.target;
+
+    if (!dropdownMenu.contains(targetElement)) {
+        dropdownMenu.style.display = "none";
+    }
+});
