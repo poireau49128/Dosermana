2f63b814213fefa38317ccb6ec9142ee6882fd99
poireau49128 <kovalkov1899@mail.ru>
2024-03-13 11:55:58 +0300

    Дизайн шапки без контроллеров на кнопках

---
+++ Dosermana.WebUI/Content/js/swiper.js
@@ -0,0 +1,13 @@
+﻿const swiper_main = new Swiper('.swiper-main', {
+    direction: 'horizontal',
+    /*loop: true,*/
+    rewind: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+});
